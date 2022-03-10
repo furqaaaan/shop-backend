6193725aec573b99b2bedc3f606ef4f497ea8041
Furqaan <mohamad.furqaan@gmail.com>
2022-03-10 17:27:30 +0800

    clean up

--- config/config.js
+++ config/config.js
@@ -24,15 +24,16 @@ const prod = {
     port: parseInt(process.env.DEV_APP_PORT) || 3000,
   },
   db: {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 3306,
-    database: process.env.DB_DATABASE || 'shop',
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSOWRD || 'password',
-    dialect: process.env.DB_DIALECT || 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSOWRD,
+    dialect: process.env.DB_DIALECT,
   },
   jwt: {
-    secret: 'anotherrandomsecret',
+    secret: process.env.JWT_SECRET,
+    expiry: 3600,
   },
 };
 
